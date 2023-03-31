1df2b31a35ec686051cd758b9ad38790c581ce4f
Anuradha203 <83829775+Anuradha203@users.noreply.github.com>
2023-03-31 11:38:11 +0530

    commit

--- MEVN/VueJS/hello-world/src/App.vue
+++ MEVN/VueJS/hello-world/src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/contact">Contact</router-link>
+    <router-view></router-view>
+  </div>
+  
 </template>
 
 <script>
@@ -14,7 +22,7 @@ export default {
 }
 </script>
 
-<style>
+<style> 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
